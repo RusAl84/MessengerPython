732a9b62aaa6d10361a71473e06395303b8f0d60
Rusal84 <rusal@bk.ru>
2021-03-17 03:11:24 +0300

    add fix

--- flaskMessenger/app.py
+++ flaskMessenger/app.py
@@ -17,21 +17,17 @@ def status():
         'messages_count': len(ListOfMessages)
     }
 
-
 # отправка сообщений
 @app.route("/api/Messanger", methods=['POST'])
 def SendMessage():
     msg = request.json
-    if len(msg['MessageText']) > 0:
-        # messages.append({'username': UserName, 'text': MessageText, 'TimeStamp': time.time()})
-        # messages.append({ "UserName":"RusAl","MessageText":"Privet na sto let!!!","TimeStamp":"2021-03-05T18:23:10.932973Z"})
-        ListOfMessages.append(msg)
-        print(msg)
-        msgtext = f"{msg['UserName']} <{msg['TimeStamp']}>: {msg['MessageText']}"
-        print(f"Всего сообщений: {len(ListOfMessages)} Посланное сообщение: {msgtext}")
-        return f"Сообщение отослано успшно. Всего сообщений: {len(ListOfMessages)} ", 200
-    else:
-        return "Record not found", 400
+    print(msg)
+    # messages.append({ "UserName":"RusAl","MessageText":"Privet na sto let!!!","TimeStamp":"2021-03-05T18:23:10.932973Z"})
+    ListOfMessages.append(msg)
+    print(msg)
+    msgtext = f"{msg['UserName']} <{msg['TimeStamp']}>: {msg['MessageText']}"
+    print(f"Всего сообщений: {len(ListOfMessages)} Посланное сообщение: {msgtext}")
+    return f"Сообщение отослано успшно. Всего сообщений: {len(ListOfMessages)} ", 200
 
 
 # получение сообщений
@@ -43,8 +39,6 @@ def GetMessage(id):
         return ListOfMessages[id], 200
     else:
         return "Not found", 400
-
-
 
 if __name__ == '__main__':
     app.run()
